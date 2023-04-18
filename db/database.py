304e0eaf49a5aacc943d35e7c00b807c5278ab4c
lucianokapluk <kaplukluciano@gmail.com>
2023-04-18 00:38:51 -0300

    add .env

--- db/database.py
+++ db/database.py
@@ -1,8 +1,20 @@
+
+
+import os
+
+from dotenv import dotenv_values
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:4636@localhost:5432/postgres"
+config_env = {
+    **dotenv_values(".env"),  # load local file development variables
+    **os.environ,  # override loaded values with system environment variables
+}
+
+SQLALCHEMY_DATABASE_URL = config_env["DATABASE_URL"]
+
+# , "postgresql://postgres:4636@localhost:5432/postgres"
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
