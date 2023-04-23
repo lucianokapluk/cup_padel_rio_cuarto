8ba28e4dfcac1e81406d772122e7a15bbbaae316
lucianokapluk <kaplukluciano@gmail.com>
2023-04-23 00:12:03 -0300

    add filter by category

--- routers/users.py
+++ routers/users.py
@@ -85,5 +85,5 @@ def delete_user(user_id: int, db: Session = Depends(get_db)):
     db_user = user_crud.get_user(db, user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
-    user_crud.delete_user(db, db_user)
+    user_crud.delete_user(db, user_id)
     return {"message": f"User {user_id} deleted successfully"}
