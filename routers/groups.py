1a3c6bd83d176c937d82698d92672f48cedfb9ec
lucianokapluk <kaplukluciano@gmail.com>
2023-06-03 17:54:35 -0300

    add reorder items position

--- routers/groups.py
+++ routers/groups.py
@@ -19,12 +19,16 @@ router = APIRouter(
 
 @router.post("/group/{group_id}/inscription")
 async def assign_inscription(group_id: int, inscription_id: int, db: Session = Depends(get_db)):
+    group: GroupModel = db.query(GroupModel).filter_by(id=group_id).first()
+
     inscription = db.query(TournamentInscriptionModel).filter_by(
         id=inscription_id).first()
     if inscription:
         group = db.query(GroupModel).filter_by(id=group_id).first()
         if group:
             inscription.group_id = group_id
+            inscription.position = len(group.inscriptions)+1
+            inscription.updated_at = datetime.utcnow()
             db.commit()
             return {"message": "Inscription assigned to group"}
         else:
@@ -80,15 +84,71 @@ async def delete_group(group_id: int, db: Session = Depends(get_db)):
 
 
 @router.put("/group/inscriptions/{inscription_id}")
-def update_inscription(inscription_id: int, position: int, db: Session = Depends(get_db)):
-    inscription = db.query(TournamentInscriptionModel).filter(
-        TournamentInscriptionModel.id == inscription_id).first()
+def update_inscription(inscription_id: int, position: int, group_id: int, db: Session = Depends(get_db)):
+    group: GroupModel = db.query(GroupModel).filter_by(id=group_id).first()
+    if group:
+        inscriptionList: List[TournamentInscription] = group.inscriptions
 
-    if inscription:
-        inscription.position = position
-        inscription.updated_at = datetime.utcnow()
-        db.commit()
-        db.refresh(inscription)
-        return {"message": "Inscription updated successfully"}
+        # Verificar si la inscripción existe en el grupo
+        target_inscription = next(
+            (inscription for inscription in inscriptionList if inscription.id == inscription_id), None)
+        if target_inscription:
+            # Obtener la posición actual de la inscripción
+            current_position = target_inscription.position
 
-    return {"message": "Inscription not found"}
+            # Si la posición deseada es menor que la posición actual, desplazar las inscripciones hacia adelante
+            if position < current_position:
+                for inscription in inscriptionList:
+                    if inscription.id != inscription_id and inscription.position >= position and inscription.position < current_position:
+                        inscription.position += 1
+                        inscription.updated_at = datetime.utcnow()
+                        db.add(inscription)
+
+            # Si la posición deseada es mayor que la posición actual, desplazar las inscripciones hacia atrás
+            elif position > current_position:
+                for inscription in inscriptionList:
+                    if inscription.id != inscription_id and inscription.position <= position and inscription.position > current_position:
+                        inscription.position -= 1
+                        inscription.updated_at = datetime.utcnow()
+                        db.add(inscription)
+
+            # Establecer la nueva posición para la inscripción actual
+            target_inscription.position = position
+            target_inscription.updated_at = datetime.utcnow()
+            db.add(target_inscription)
+
+            db.commit()  # Confirmar la transacción después de realizar
+
+            return {"message": "Inscription updated successfully"}
+        else:
+            return {"message": "Inscription not found"}
+    else:
+        return {"message": "Group not found"}
+
+
+@router.put("/group/{group_id}/reorder")
+def remove_inscription_and_update_positions(group_id: int, db: Session = Depends(get_db)):
+    group: GroupModel = db.query(GroupModel).filter_by(id=group_id).first()
+    if group:
+        inscriptionList: List[TournamentInscription] = group.inscriptions
+        if len(inscriptionList) != 0:
+            # Restar 1 a todas las posiciones, evitando que haya saltos en los números de posición
+            sorted_inscriptions = sorted(
+                inscriptionList, key=lambda x: x.position)
+            new_positions = [index + 1 for index,
+                             _ in enumerate(sorted_inscriptions)]
+            position_mapping = dict(zip(sorted_inscriptions, new_positions))
+
+            for inscription in inscriptionList:
+                new_position = position_mapping[inscription]
+                inscription.position = new_position
+                inscription.updated_at = datetime.utcnow()
+                db.add(inscription)
+
+            db.commit()  # Confirmar la transacción después de realizar todas las actualizaciones
+
+            return {"message": "Positions updated successfully"}
+        else:
+            return {"message": "Lista vacia"}
+    else:
+        return {"message": "Group not found"}
